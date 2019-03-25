4e045cf088133b7c01358aed5ac6d7da9599344a
Armando Morales <amorales944@gmail.com>
2019-03-25 06:37:43 -0500

    Latest updates

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,4 +1,50 @@
-$(document).ready(function() {
 
+
+
+
+$(document).ready(function() {
+    var correct = 0;
+
+
+
+
+
+    $("#buttonId").on("click", function() {
+    var questionCheck = document.getElementById("#questionCheck").value;
+    questionCheck === question1A
+        correct++;
+        console.log(correct);
+ 
+// //  if (question2) {
+// //     correct++
+// //  }
+// //  if (question3) {
+// //     correct++
+// //  }
+// //  if (question4) {
+// //     correct++
+// //  }
+// //  if (question5) {
+// //     correct++
+// //  }
+// //  if (question6) {
+// //     correct++
+// //  }
+// //  if (question7) {
+// //     correct++
+// //  }
+// //  if (question8) {
+// //     correct++
+// //  }
+// //  if (question9) {
+// //     correct++
+// //  }
+// //  if (question10) {
+// //     correct++
+//  }// else if (questionCheck === ) {
+
+// // } else {
+
+});
 
 });
